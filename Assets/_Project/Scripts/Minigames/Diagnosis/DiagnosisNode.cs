c90670dd9821bb0bf5007fbac9268bcc6f97cac6
SonNPXSE183201 <SonNPXSE183201@fpt.edu.vn>
2026-07-09 15:01:17 +0700

    feat: package diagnosis minigame as a standalone prefab

---
+++ Assets/_Project/Scripts/Minigames/Diagnosis/DiagnosisNode.cs
@@ -0,0 +1,137 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+namespace Minigames.Diagnosis
+{
+    public enum NodeState
+    {
+        Unprobed,
+        ProbedNormal,
+        ProbedWarning,
+        ProbedFault
+    }
+
+    /// <summary>
+    /// Gắn script này vào UI Button hoặc GameObject 3D (có Collider) đại diện cho điểm đo trên mạch.
+    /// </summary>
+    public class DiagnosisNode : MonoBehaviour, IPointerClickHandler
+    {
+        [Header("Node Info")]
+        [Tooltip("ID của điểm đo (VD: Node_1) để đối chiếu với lỗi")]
+        [SerializeField] private string nodeId;
+        
+        [Header("Visual Feedback")]
+        [SerializeField] private Color unprobedColor = Color.white;
+        [SerializeField] private Color normalColor = Color.green;
+        [SerializeField] private Color warningColor = Color.yellow;
+        [SerializeField] private Color faultColor = Color.red;
+
+        // Cho 2D UI
+        private Image _nodeImage;
+        private Button _nodeButton;
+        
+        // Cho 3D Object
+        private MeshRenderer _meshRenderer;
+
+        private NodeState _currentState = NodeState.Unprobed;
+        private bool _hasFault = false;
+
+        public string NodeId { get => nodeId; set => nodeId = value; }
+        public NodeState CurrentState => _currentState;
+        public bool HasFault => _hasFault;
+
+        public event Action<DiagnosisNode> OnNodeProbed;
+
+        private void Awake()
+        {
+            _nodeImage = GetComponent<Image>();
+            _nodeButton = GetComponent<Button>();
+            _meshRenderer = GetComponent<MeshRenderer>();
+            
+            // Nếu có UI Button thì lắng nghe sự kiện
+            if (_nodeButton != null)
+            {
+                _nodeButton.onClick.AddListener(ProbeNode);
+            }
+        }
+
+        public void Setup(string id, bool hasFault)
+        {
+            this.nodeId = id;
+            this._hasFault = hasFault;
+            this._currentState = NodeState.Unprobed;
+            UpdateVisuals();
+        }
+
+        // Dành cho UI (nếu không dùng Button mà chỉ dùng EventSystem)
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (_nodeButton == null) // Tránh gọi 2 lần nếu đã có Button hook
+            {
+                ProbeNode();
+            }
+        }
+
+        // Dành cho 3D Object (Yêu cầu có Collider gắn trên object)
+        private void OnMouseDown()
+        {
+            ProbeNode();
+        }
+
+        /// <summary>
+        /// Xử lý logic khi người chơi click/chạm vào node.
+        /// </summary>
+        public void ProbeNode()
+        {
+            if (_currentState != NodeState.Unprobed) return;
+
+            _currentState = _hasFault ? NodeState.ProbedFault : NodeState.ProbedNormal;
+            UpdateVisuals();
+            OnNodeProbed?.Invoke(this);
+        }
+
+        public void SetWarning()
+        {
+            if (_currentState == NodeState.ProbedNormal)
+            {
+                _currentState = NodeState.ProbedWarning;
+                UpdateVisuals();
+            }
+        }
+
+        private void UpdateVisuals()
+        {
+            Color targetColor = unprobedColor;
+            switch (_currentState)
+            {
+                case NodeState.Unprobed:
+                    targetColor = unprobedColor;
+                    break;
+                case NodeState.ProbedNormal:
+                    targetColor = normalColor;
+                    break;
+                case NodeState.ProbedWarning:
+                    targetColor = warningColor;
+                    break;
+                case NodeState.ProbedFault:
+                    targetColor = faultColor;
+                    break;
+            }
+
+            // Đổi màu cho 2D UI
+            if (_nodeImage != null)
+            {
+                _nodeImage.color = targetColor;
+            }
+            
+            // Đổi màu cho 3D Object
+            if (_meshRenderer != null)
+            {
+                // Thay đổi màu của Material (Tạo ra bản sao của Material để không ảnh hưởng các object khác)
+                _meshRenderer.material.color = targetColor;
+            }
+        }
+    }
+}
